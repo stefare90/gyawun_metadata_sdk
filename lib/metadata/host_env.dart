aed4ee8183a8c071b88b350bcb7eebff180c804f
ares <stefare@hotmail.it>
2026-07-29 17:34:53 +0200

    Added sdk version to HostEnv

--- lib/metadata/host_env.dart
+++ lib/metadata/host_env.dart
@@ -2,9 +2,12 @@ import 'package:eval_annotation/eval_annotation.dart';
 import 'package:gyawun_metadata_sdk/metadata/interfaces/inetwork_service.dart';
 import 'package:gyawun_metadata_sdk/metadata/interfaces/istorage_service.dart';
 import 'package:gyawun_metadata_sdk/metadata/interfaces/iui_service.dart';
+import 'package:gyawun_metadata_sdk/src/version.dart';
 
 @Bind()
 class HostEnv {
+  static const String sdkVersion = packageVersion;
+
   final INetworkService network;
   final IStorageService storage;
   final IUIService ui;
